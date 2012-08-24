4475ab28eaca50c57df553bba3790af1b2b5648e
jobby <inverted+github@gmail.com>
2012-08-24 16:23:37 +0100

    Solution to 31
    Brute force!

---
+++ python/problem31.py
@@ -0,0 +1,13 @@
+t = 200
+n = 0
+
+for a in range(t, -1, -200):
+    for b in range(a, -1, -100):
+        for c in range(b, -1, -50):
+            for d in range(c, -1, -20):
+                for e in range(d, -1, -10):
+                    for f in range(e, -1, -5):
+                        for g in range(f, -1, -2):
+                            n += 1
+
+print n
